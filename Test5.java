7d553f1dfc83fc59b5c4f80096d8a256e8ffc19a
AkhilaReddy24 <34599098+AkhilaReddy24@users.noreply.github.com>
2017-12-17 13:23:25 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Test5
+{
+public static void main(String[] args)
+{
+Scanner input=new Scanner(System.in);
+System.out.println("Enter a number");
+int i=input.nextInt();
+while(i<=100)
+{
+
+/*if(i<=100)
+{*/
+if(i==24)
+{
+System.out.println("valid number");
+break;
+}
+else
+{
+System.out.println("not a valid number");
+break;
+}
+}
+}
+}
+
---
+++ Test6.java
@@ -0,0 +1,11 @@
+public class Test6
+{
+public static void main(String[] args)
+{
+int i,j;
+i=(10<<1)+10;
+j=10>>1;
+System.out.println("multiplication of 10 and 3 is:"+i);
+System.out.println("division of 10 and 2 is:"+j);
+}
+}
